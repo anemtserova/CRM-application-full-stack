80594f02de0e00edb311fc8ae846b8af1e6e8abe
Antoniya <anemtserova@gmail.com>
2021-09-12 17:49:54 +0000

    store/context update

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,49 +1,95 @@
-const getState = ({ getStore, getActions, setStore }) => {
+const getState = ({ getStore, setStore, getActions }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			editFetch: monkey => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + monkey.kite, {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						full_name: monkey.full_name,
+						email: monkey.email,
+						agenda_slug: "agenda_2021",
+						address: monkey.address,
+						phone: monkey.phone
+					})
+				})
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => {
+						getActions().getFetch();
+						// confirm return of data here
+					})
+					.catch(err => console.log("There was a following error: " + err));
 			},
 
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
+			//(Arrow) Functions that update the Store
+			// Remember to use the scope: scope.state.store & scope.setState()
+			deleteFetch: id => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+					method: "DELETE"
+				})
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => {
+						getActions().getFetch();
+						// confirm return of data here
+					})
+					.catch(err => console.log("There was a following error: " + err));
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			getFetch: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/agenda_2021")
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => setStore({ contacts: data }))
+					.catch(err => console.log("There was a following error: " + err));
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
+			postFetch: contact => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						full_name: contact.name,
+						email: contact.email,
+						agenda_slug: "agenda_2021",
+						address: contact.address,
+						phone: contact.phone
+					})
+				})
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => {
+						getActions().getFetch();
+						// confirm return of data here
+					})
+					.catch(err => console.log("There was a following error: " + err));
 			}
 		}
 	};
 };
-
 export default getState;
