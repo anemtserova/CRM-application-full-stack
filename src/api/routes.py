feac3bf69867a2bbb7f01e12448ef9e297f8545a
Antoniya <anemtserova@gmail.com>
2021-09-13 20:32:44 +0000

    still not working

--- src/api/routes.py
+++ src/api/routes.py
@@ -18,7 +18,7 @@ api = Blueprint('api', __name__)
 # Create a route to authenticate your users and return JWTs. The
 # create_access_token() function is used to actually generate the JWT.
 @api.route("/token", methods=["POST"])
-def create_login_token():
+def create_token():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     if username != "test" or password != "test":
