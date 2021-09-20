3429020e6f617d6527606c4e4517164da8d4d333
Antoniya <anemtserova@gmail.com>
2021-09-20 14:40:35 +0000

    login update

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -33,33 +33,34 @@ export const Login = () => {
 
 	return (
 		<div className="container-fluid d-flex flex-column align-items-center ">
-					<div className="d-flex justify-content-center w-25">
-						<h1>User Login</h1>
-					</div>
+			<div className="d-flex justify-content-center w-25">
+				<h1>User Login</h1>
+			</div>
 			{token && token != "" && token != undefined ? (
-				<h2>`You are logged in with token ${token}`</h2>
+				<>
+					<h2>You are logged in with token</h2>
+					<h4 className="w-50">{username}</h4>
+				</>
 			) : (
-				
-					<div className="d-flex flex-column justify-content-center w-25">
-						<input
-							type="text"
-							placeholder="username"
-							value={username}
-							onChange={e => setUsername(e.target.value)}
-							className="m-1"
-						/>
-						<input
-							type="password"
-							placeholder="password"
-							value={password}
-							onChange={e => setPassword(e.target.value)}
-							className="m-1"
-						/>
-						<button onClick={handleLogin} type="submit" className="m-1 btn btn-secondary">
-							Login
-						</button>
-					</div>
-				
+				<div className="d-flex flex-column justify-content-center w-25">
+					<input
+						type="text"
+						placeholder="username"
+						value={username}
+						onChange={e => setUsername(e.target.value)}
+						className="m-1"
+					/>
+					<input
+						type="password"
+						placeholder="password"
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+						className="m-1"
+					/>
+					<button onClick={handleLogin} type="submit" className="m-1 btn btn-secondary">
+						Login
+					</button>
+				</div>
 			)}
 		</div>
 	);
