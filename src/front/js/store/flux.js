892aa7b04a135b94a2c09612fe0e44ece1fa4b90
Antoniya <anemtserova@gmail.com>
2021-10-05 10:49:56 +0000

    updates

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -45,7 +45,7 @@ const getState = ({ getStore, setStore, getActions }) => {
 					})
 				};
 				try {
-					const resp = await fetch("https://3001-apricot-gull-2kgbaff4.ws-eu18.gitpod.io/api/token", opts);
+					const resp = await fetch(process.env.BACKEND_URL + "/api/token", opts);
 					if (resp.status !== 200) {
 						alert("There has been an error.");
 						return false;
@@ -102,7 +102,7 @@ const getState = ({ getStore, setStore, getActions }) => {
 					}
 				};
 
-				fetch("https://3001-apricot-gull-2kgbaff4.ws-eu18.gitpod.io/api/greet", opts)
+				fetch(process.env.BACKEND_URL + "/api/greet", opts)
 					.then(resp => resp.json())
 					.then(data => setStore({ message: data.message }))
 					.catch(err => console.log("There has been an error loading message from backend", err));
