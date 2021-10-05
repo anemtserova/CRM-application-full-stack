dee11eb4c3fb1518fac4916ad9b23452b6850c57
Antoniya <anemtserova@gmail.com>
2021-10-05 11:51:43 +0000

    updates

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -19,16 +19,25 @@ export const Home = () => {
 	);
 
 	return (
-		<div className="container">
+		<div className="container mb-3">
 			{!store.token ? (
-				<>
+				<div className="d-flex flex-column align-items-center mb-3">
 					<p className="heading-1"> Welcome to KeepIt.App</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Nec feugiat nisl pretium fusce id velit ut. Risus quis varius
+						quam quisque id diam vel quam. Amet nisl suscipit adipiscing bibendum est ultricies integer
+						quis. Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue. Suscipit tellus mauris a
+						diam. Suspendisse potenti nullam ac tortor vitae. Sodales ut eu sem integer vitae justo. Arcu ac
+						tortor dignissim convallis aenean et. A arcu cursus vitae congue mauris rhoncus aenean vel. Eu
+						nisl nunc mi ipsum. Rhoncus dolor purus non enim praesent elementum facilisis leo.
+					</p>
 					<Link to="/login">
-						<button className="btn btn-style">Log In Here</button>
+						<button className="btn btn-style ">Log In Here</button>
 					</Link>
-				</>
+				</div>
 			) : (
-				<div>
+				<div className="d-flex flex-column align-items-center ">
 					<p className="heading-1">{store.message}</p>
 					<Link className="heading-3" to="/contacts">
 						Go to Your Contacts
--- src/front/js/pages/logout.js
+++ src/front/js/pages/logout.js
@@ -9,32 +9,9 @@ export const Logout = () => {
 	const { store, actions } = useContext(GlobalState);
 
 	return (
-		<div className="container">
+		<div className="container d-flex flex-column align-items-center">
 			<p className="heading-1">Logged out</p>
-			{/* <ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul> */}
+
 			<br />
 			<Link to="/">
 				<button className="btn btn-style">Back to Home</button>
